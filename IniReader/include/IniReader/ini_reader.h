12fd04747cf5006b9971b009e2a10dd5ae51dbc9
Michael Amaya <mikeamaya8@gmail.com>
2021-06-07 00:13:11 -0400

    Add Basic Reader funcionality

--- IniReader/include/IniReader/ini_reader.h
+++ IniReader/include/IniReader/ini_reader.h
@@ -2,13 +2,18 @@
 
 #include <string>
 #include <fstream>
+#include <vector>
 #include <map>
 
 class IniReader {
     private:
-    std::string fileName;
-    std::map<std::string, std::string> *contents;
-    std::ifstream iniFile;
+    std::string fileName {};
+    std::map<std::string, std::string> contents {};
+    std::vector<std::string> iniContents {};
+    std::ifstream iniFile {};
+    bool parseIni();
+    bool readIni();
+    void printContents();
     public:
     IniReader(std::string fileName) noexcept(false);
     ~IniReader();
--- IniReader/src/ini_reader.cpp
+++ IniReader/src/ini_reader.cpp
@@ -2,14 +2,42 @@
 #include <IniReader/ini_reader.h>
 
 IniReader::IniReader(std::string file) : fileName{file} {
+    // Check if the file exists, throw if it doesn't
     iniFile.open(fileName);
     if (!iniFile.is_open())
         throw std::runtime_error(fileName + " does not exist");
 
-    // Do operations 
+    // Read the contents and close the file
+    readIni();
     iniFile.close();
+
+    // Parse the data to populate the contents map
+    parseIni();
+
+    // Print the contents of the iniContents vector which
+    // just contains each line
 }
 
 IniReader::~IniReader(){
-    delete contents;
+    // delete contents;
+}
+
+void IniReader::printContents() {
+    for (auto i = iniContents.begin(); i != iniContents.end(); ++i) {
+        std::cout << *i << '\n';
+    }
+}
+
+bool IniReader::parseIni() {
+
+    return true;
+}
+
+bool IniReader::readIni() {
+    std::string line {}; 
+    while (std::getline(iniFile, line)) {
+        iniContents.push_back(line);
+    }
+
+    return true;
 }
