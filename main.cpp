95d2f5bbeb86f614b1eb687bd92f4e33bcbd5b7f
Michael Amaya <mikeamaya8@gmail.com>
2021-06-15 22:07:30 -0400

    Add simple test to get data from the test.ini file

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <IniReader/ini_reader.h>
 
 int main (int argc, char **argv) {
-    std::cout << "Hello World!" << std::endl;
     IniReader reader {"test.ini"};
+    std::cout << "RSA-Directory:" << reader.getSection("Random")->at("Rsa-Directory") << std::endl;
     return 0;
 }
